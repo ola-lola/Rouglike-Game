21c680e148dbe7949ca7230732ab607a5318391c
AgnieszkaAnd <ag2rio@gmail.com>
2020-01-13 22:56:52 +0100

    create_board: simple version added + display_board added

--- engine.py
+++ engine.py
@@ -1,4 +1,14 @@
 def create_board(width, height):
+    board = []
+    for x in range(height):
+        row = []
+        for y in range(width):
+            if x < 1 or x == (height - 1) or y < 1 or y == (width - 1):
+                row.append('#')
+            else:
+                row.append(' ')
+        board.append(row)
+    return board
     '''
     Creates a new game board based on input parameters.
 
@@ -8,6 +18,20 @@ def create_board(width, height):
 
     Returns:
     list: Game board
+    '''
+
+
+def verify_move_is_possible(key_input, board, player):
+    '''
+    Modifies player coordinates if allowed (player cannot move through walls etc)
+
+    Args:
+    key_input: input string 'w', 's', 'a', 'd' etc.
+    list: The game board
+    dictionary: The player information containing the icon and coordinates
+
+    Returns:
+    Nothing
     '''
     pass
 
--- ui.py
+++ ui.py
@@ -1,8 +1,11 @@
 def display_board(board):
+    for line in board:
+        for character in line:
+            print(character, end='')
+        print()
     '''
     Displays complete game board on the screen
 
     Returns:
     Nothing
     '''
-    pass
