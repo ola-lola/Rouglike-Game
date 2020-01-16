614709652dcc05d559adfc63dbb6b192774f01c8
Vig1lante <apakieser@gmail.com>
2020-01-16 14:58:56 +0100

    Add monsters file

---
+++ monsters.py
@@ -0,0 +1,33 @@
+import util
+import engine
+import ui
+import items
+# Import modules here, got all of them so far, will cut out in final version of game
+
+# Define monsters attrbts/inventory function, needed as a dict with constant data when imported for operations in other modules
+def monsters_overview():
+    monsters = {
+        "green": {
+            "icon": "G",
+            "damage": 10,
+            "hps": 50,
+            "experience": 25,
+            "inventory": items.mob_inv(items.items_list(),items.greenItems)
+            },
+        "yellow": {
+            "icon": "Y",
+            "damage": 10,
+            "hps": 50,
+            "experience": 25,
+            "inventory": items.mob_inv(items.items_list(), items.yellowItems)
+            },
+        "red": {
+            "icon": "R",
+            "damage": 10,
+            "hps": 50,
+            "experience": 25,
+            "inventory": items.items_list()
+            }
+    }
+    return monsters
+
