0ea17b4eb804a6e433e2c5bc5f588a2724e446a0
AgnieszkaAnd <ag2rio@gmail.com>
2020-01-16 20:01:55 +0100

    file operations added import/export

---
+++ file_operations.py
@@ -0,0 +1,25 @@
+def import_board(filename):
+    try:
+        with open(filename, mode='r') as file:
+            file_content = file.readlines()
+        for i, line in enumerate(file_content):
+            file_content[i] = line.strip(",\n").split(",")
+        return file_content
+    except FileNotFoundError:
+        print(f"File '{filename}' not found!")
+
+
+def export_board(board_state, filename):
+    # STEP 1: prepare a string with data to save to the file
+    string_to_export = ""
+    for line in board_state:
+        for character in line:
+            string_to_export += f"{character},"
+        string_to_export += "\n"
+    # STEP 2: write data to file in csv format + catch PermissionError if no access to filename
+    try:
+        with open(filename, mode='w') as file:
+            file.write(string_to_export)
+    except PermissionError:
+        print(f"You don't have permission creating file '{filename}'!")
+    
