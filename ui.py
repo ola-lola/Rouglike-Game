38526091bde12399db435ace12a08fc584c49c8f
Ola <olagrzegorczyk93@gmail.com>
2020-01-17 18:34:08 +0100

    Small changes in print inventory

--- ui.py
+++ ui.py
@@ -9,17 +9,7 @@ def display_board(board):
 
 
 def calculate_max_column_width(column_name, column_data):
-    """
-    Calculate max column width for table printing purposes
-    in order to assure easily readable table print
-
-    Args:
-        column_name (string): name of the column intended to use in the table print
-        column_data (any type): iterable or non-iterable data container with data to print
-
-    Returns:
-        max_column_width (int): number of characters in column width
-    """
+   
     max_column_width = 0
 
     if max_column_width <= len(column_name):
@@ -38,21 +28,14 @@ def calculate_max_column_width(column_name, column_data):
 
 
 def print_table(player):
-    """
-    Display the contents of the inventory in an ordered, well-organized table with
-    each column right-aligned.
-    """
-    
-    # Calculate max char size for left and right col
+   
     max_lcol = calculate_max_column_width('Inventory', player)
     max_rcol = calculate_max_column_width('Inventory', player.values())
 
-    # Print table top row (numbers are extra spaces(for separator/white space) for proper formatting)
     print((max_lcol + 1) * "-" + (max_lcol + 2) * "-")
     print(f"{'~ WEAPON ~':^{max_lcol}} | {'count':^{max_lcol}}")
     print((max_lcol + 1) * "-" + (max_lcol + 2) * "-")
 
-    # Print properly formatted key-value pair in table
     for k, v in player['Inventory']['weapon items'].items():
         print(f"{k:^{max_lcol}} | {v:^{max_lcol}}")
     print((max_lcol + 1) * "-" + (max_lcol + 2) * "-")
@@ -60,7 +43,7 @@ def print_table(player):
     print(f"{'~ FOOD ~':^{max_lcol}} | {'count':^{max_lcol}}")
     print((max_lcol + 1) * "-" + (max_lcol + 2) * "-")
 
-    # Print properly formatted key-value pair in table
+
     for k, v in player['Inventory']['food items'].items():
         print(f"{k:^{max_lcol}} | {v:^{max_lcol}}")
     print((max_lcol + 1) * "-" + (max_lcol + 2) * "-")
@@ -69,7 +52,7 @@ def print_table(player):
     print(f"{'~ *** ~':^{max_lcol}} | {'count':^{max_lcol}}")
     print((max_lcol + 1) * "-" + (max_lcol + 2) * "-")
 
-    # Print properly formatted key-value pair in table
+
     for k, v in player['Inventory']['special items'].items():
         print(f"{k:^{max_lcol}} | {v:^{max_lcol}}")
     print((max_lcol + 1) * "-" + (max_lcol + 2) * "-")
